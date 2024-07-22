0ea4489f1aa9bf7518d8be49bdc838fe60107f34
zhangkai <2572429235@qq.com>
2024-07-22 08:11:09 +0800

    zhangkai
    fix: 初始化

---
+++ src/main/java/cn/com/v2/controller/GoviewProjectDataController.java
@@ -0,0 +1,19 @@
+package cn.com.v2.controller;
+
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * <p>
+ *  前端控制器
+ * </p>
+ *
+ * @author fc
+ * @since 2023-04-30
+ */
+@RestController
+@RequestMapping("/v2/goview-project-data")
+public class GoviewProjectDataController {
+
+}
